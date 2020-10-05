21931a1c612b9f9b12602279e75e28a26f0f660e
naing <naing@KoNaing>
2020-10-05 16:00:24 +0630

    First Profect

---
+++ test/src/com/fes/test/Test.java
@@ -0,0 +1,14 @@
+package com.fes.test;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+loop();
+	}
+
+     public static void loop() {
+    	 System.out.println("Hello");
+     }
+    	
+     }
